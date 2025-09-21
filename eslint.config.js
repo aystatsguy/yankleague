fd0bdbb5a8e0b838f3f942b984e97012965e9619
aystatsguy <adamystatsguy@gmail.com>
2025-09-21 10:10:01 -0400

    Fix directory navigation and ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,11 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default [
   { ignores: ['dist'] },
@@ -16,7 +21,7 @@ export default [
       sourceType: 'module',
       parserOptions: {
         project: './tsconfig.app.json',
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
       },
     },
     plugins: {
